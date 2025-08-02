e53b2c5fe58b70ee19288586df21d35bd93eff13
BurgosManuel <trapecioinv@gmail.com>
2025-08-02 02:25:29 -0300

    Estructura inicial del proyecto para TFI

---
+++ tfi_ordenamiento.cpp
@@ -0,0 +1,122 @@
+#include <stdio.h> // Libreria estandar para usar printf() y scanf().
+#include <stdlib.h> // Libreria para hacer uso de la funcion rand().
+
+// Variables globales
+const char *metodos[] = { // Guardamos los metodos en un arreglo para poder generar las opciones dinamicamente y acceder a sus valores a traves de un indice.
+    "Bubble Sort",
+    "Selection Sort",
+    "Insertion Sort",
+    "MergeSort",
+    "QuickSort"
+};
+
+int cOpciones = 5; // Podriamos obtener la cantiodad de opciones de forma dinamica con sizeof(), pero como sabemos que es estatica, directamente la declararemos.
+int min = 5, max = 25; // Variables globales para controlar la cantidad de elementos minimos y maximos en el vector.
+
+// Prototipo de funciones
+int elegirMetodo();
+void cargarAleatorio(int v[], int &n);
+void imprimirVector(int v[], int n);
+void ejecutarMetodo(int v[], int n, int codMetodo);
+
+void bubbleSort(int v[], int n);
+void selectionSort(int v[], int n);
+void insertionSort(int v[], int n);
+void mergeSort(int v[], int n);
+void quickSort(int v[], int n);
+
+main() {
+	printf("========== TRABAJO FINAL INTEGRADOR - AyED 2025 ==========\n");
+	int v[100], n;
+	cargarAleatorio(v, n);
+	
+	printf("El vector generado es: \n");
+	imprimirVector(v, n);
+	
+	int codMetodo = elegirMetodo();
+	ejecutarMetodo(v, n, codMetodo);
+}
+
+void cargarAleatorio(int v[], int &n) {
+	do {
+		printf("Ingrese cantidad de elementos para el vector (entre %d y %d):", min, max);
+		scanf("%d", &n);
+		if(n < min || n > max) {
+			printf("ERROR: Ingrese un valor entre %d y %d.\n", min, max);
+		}
+	} while (n < min || n > max);
+
+	
+	for (int i = 0; i < n; i++) {
+		v[i] = rand() % 100; // Asignamos un valor aleatorio entre 0 y 99
+	}
+}
+
+void imprimirVector(int v[], int n) {
+	printf("[");
+	for (int i = 0; i < n; i++) {
+		// Imprimimos el valor en la misma linea, y si no es el ultimo elemento, agregamos una coma.
+		printf("%d", v[i]);
+		if (i != n - 1) {
+			printf(",")	;
+		}
+	}
+	printf("]\n");
+}
+
+int elegirMetodo() {
+	int opcion;
+	
+	printf("========== METODOS DE ORDENAMIENTO ==========\n");
+	for (int i = 0; i < cOpciones; i++) { // Generamos las opciones de forma dinamica.
+		printf("[%d]. %s\n", i, metodos[i]);
+	}
+    
+    do {
+    	printf("Seleccione una opcion: ");
+    	scanf("%d", &opcion);
+    	if(opcion < 0 || opcion > 4){
+			printf("El metodo elegido no existe, seleccione uno de la lista.\n");
+		}
+	} while(opcion < 1 || opcion > 4);
+    
+	return opcion;
+}
+
+void ejecutarMetodo(int v[], int n, int codMetodo) {
+	printf("El metodo elegido es: %s.\n", metodos[codMetodo]);
+	
+    switch (codMetodo) {
+        case 0:
+            bubbleSort(v, n);
+            break;
+        case 1:
+            selectionSort(v, n);
+            break;
+        case 2:
+            insertionSort(v, n);
+            break;
+        case 3:
+            mergeSort(v, n);
+            break;
+        case 4:
+            quickSort(v, n);
+            break;
+    }
+}
+
+void bubbleSort(int v[], int n) {
+	printf("Ordenado con bubbleSort\n");
+}
+void selectionSort(int v[], int n) {
+	printf("Ordenado con selectionSort\n");
+}
+void insertionSort(int v[], int n) {
+	printf("Ordenado con insertionSort\n");
+}
+void mergeSort(int v[], int n) {
+	printf("Ordenado con mergeSort\n");
+}
+void quickSort(int v[], int n) {
+	printf("Ordenado con quickSort\n");
+}
